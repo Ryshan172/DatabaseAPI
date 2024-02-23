e0dab06eb3f445e74b61ea355b9c367c5ad5388c
Ryshan Ramlall <Ryshan.Ramlall@bbd.co.za>
2024-02-23 13:07:09 +0200

    Added constraints

--- UpdatedAllTables.sql
+++ UpdatedAllTables.sql
@@ -1,4 +1,5 @@
 --- Run this script with SSMS to create the database 
+-- For Azure DB, Take out Use master and Use BursaryDatabase, since you should already be in the DB 
 USE master;  
 GO  
 CREATE DATABASE BursaryDatabase 
@@ -12,7 +13,7 @@ GO
 /*Updated Roles Table*/
 -- Creating Roles Table 
 CREATE TABLE [dbo].Roles (
-RoleID INT PRIMARY KEY IDENTITY(1,1) ,
+RoleID INT PRIMARY KEY CLUSTERED IDENTITY(1,1) ,
 RoleName varchar(10),
 );
 GO
@@ -21,7 +22,7 @@ GO
 
 -- Creating Users Table 
 CREATE TABLE [dbo].Users (
-    UserID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     FirstName VARCHAR(255) NOT NULL,
     LastName VARCHAR(255) NOT NULL,
     RoleID INT NOT NULL,
@@ -31,7 +32,7 @@ GO
 
 -- Creating Reviewers Table 
 CREATE TABLE [dbo].Reviewers (
-    ReviewerID INT PRIMARY KEY IDENTITY(1,1),
+    ReviewerID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     UserID INT NOT NULL,
     StudentAllocationID INT,
     UniversityID INT,
@@ -41,8 +42,8 @@ CREATE TABLE [dbo].Reviewers (
 )
 
 /*Contact Details Table*/
-CREATE TABLE ContactDetails (
-    ContactID INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE [dbo].ContactDetails (
+    ContactID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     UserID INT NOT NULL,
     Email VARCHAR(255) ,
     PhoneNumber VARCHAR(20),
@@ -51,15 +52,15 @@ CREATE TABLE ContactDetails (
 
 /*Added Ethnicity, Dept and Genders tables*/
 CREATE TABLE [dbo].Ethnicity(
-EthnicityID INT PRIMARY KEY IDENTITY(1,1) ,
+EthnicityID INT PRIMARY KEY CLUSTERED IDENTITY(1,1) ,
 Ethnicity VARCHAR(8)
 );
 GO
-INSERT INTO dbo.Ethnicity(Ethnicity) VALUES ('African'),('Indian'),('Colored')
+INSERT INTO [dbo].Ethnicity(Ethnicity) VALUES ('African'),('Indian'),('Colored')
 GO
 
 CREATE TABLE [dbo].Genders(
-GenderID INT PRIMARY KEY IDENTITY(1,1) ,
+GenderID INT PRIMARY KEY CLUSTERED IDENTITY(1,1) ,
 Gender VARCHAR(6)
 );
 GO
@@ -67,7 +68,7 @@ INSERT INTO dbo.Genders(Gender) VALUES ('Female'),('Male')
 
 
 CREATE TABLE [dbo].Departments(
-DepartmentID INT PRIMARY KEY IDENTITY(1,1) ,
+DepartmentID INT PRIMARY KEY CLUSTERED IDENTITY(1,1) ,
 Department varchar(7)
 );
 GO
@@ -76,13 +77,11 @@ GO
 
 -- Creating Universities Table 
 CREATE TABLE [dbo].[Universities] (
-    [UniversityID] INT PRIMARY KEY IDENTITY(1,1),
+    [UniversityID] INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     [UniName] VARCHAR(200),
 );
 GO
-/*Foreign Key Conflict when trying to Insert a University. This also Affects Adding Students
-INSERT INTO dbo.Universities (UniName, DepartmentID, UserID) VALUES ('Wits', 1, 2);
-*/
+
 
 CREATE TABLE [dbo].[UniversityUser] (
     DepartmentID INT REFERENCES [dbo].Departments(DepartmentID) ,
@@ -93,8 +92,8 @@ GO
 
 
 --Create aplication status table
-CREATE TABLE ApplicationStatuses (
-    StatusID INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE [dbo].ApplicationStatuses (
+    StatusID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     StatusName VARCHAR(50) NOT NULL
 );
 INSERT INTO dbo.ApplicationStatuses (StatusName) 
@@ -103,19 +102,21 @@ VALUES
     ('Accepted'),
     ('Rejected');
 
+
 -- Creating University Application Table
 CREATE TABLE [dbo].[UniversityApplication] (
-    ApplicationID INT PRIMARY KEY IDENTITY(1,1),
+    ApplicationID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
     ApplicationStatusID INT,
     AmountRequested money,
     UniversityID INT REFERENCES Universities(UniversityID),
     IsLocked BIT NOT NULL DEFAULT 0, -- Adding IsLocked field, Default 0 = false
-    CONSTRAINT FK_ApplicationStatus FOREIGN KEY (ApplicationStatusID) REFERENCES ApplicationStatuses(StatusID)
+    CONSTRAINT FK_ApplicationStatus FOREIGN KEY (ApplicationStatusID) REFERENCES ApplicationStatuses(StatusID),
+    CONSTRAINT CHK_AmountAllocVal CHECK (AmountRequested >= 0) -- no negatives 
 );
 
 -- Creating Bursary Allocations Table
 CREATE TABLE BursaryAllocations (
-    AllocationID INT PRIMARY KEY IDENTITY,
+    AllocationID INT PRIMARY KEY CLUSTERED IDENTITY,
     UniversityID INT,
     AmountAlloc MONEY,
     AllocationYear INT,
@@ -127,15 +128,15 @@ GO
 
 -- Creating BBDAdminBalance Table
 CREATE TABLE [dbo].BBDAdminBalance(
-BalanceID INT PRIMARY KEY IDENTITY(1,1),
+BalanceID INT PRIMARY KEY CLUSTERED IDENTITY(1,1),
 Budget money,
 AmountRemaining AS (Budget - AmountAllocated),
 AmountAllocated money,
 BudgetYear INT -- Added year for budget 
+CONSTRAINT CHK_NonNegativeMoney CHECK (Budget >= 0 AND AmountAllocated >= 0), -- Constraint to prevent negative money values
+CONSTRAINT CHK_BudgetYear CHECK (BudgetYear >= 0)
 );
 GO
-
-
 
 
 
@@ -143,26 +144,29 @@ GO
 -- Creating Students Table
 CREATE TABLE [dbo].StudentsTable(
 	 -- Remove IDENTITY property. Can not manually enter ID 
-    StudentIDNum CHAR(13) PRIMARY KEY -- StudentID as char of size 13
+    StudentIDNum CHAR(13) PRIMARY KEY CLUSTERED, -- StudentID as char of size 13
     UserID INT NOT NULL,
-    DateOfBirth date NOT NULL,
+    DateOfBirth DATE NOT NULL,
     GenderID INT REFERENCES Genders(GenderID), -- Assuming Genders table exists
     EthnicityID INT REFERENCES [dbo].Ethnicity(EthnicityID),
-    DepartmentID INT REFERENCES [dbo].Departments(DepartmentID) ,
+    DepartmentID INT REFERENCES [dbo].Departments(DepartmentID),
     UniversityID INT REFERENCES [dbo].Universities(UniversityID), -- New column
-    CONSTRAINT FK_StudentUser FOREIGN KEY (UserID) REFERENCES Users(UserID)
+    CONSTRAINT FK_StudentUser FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    CONSTRAINT CHK_DateOfBirth CHECK (DateOfBirth <= SYSDATETIME()) -- Constraint to prevent future DOB
 );
 
 
 -- Creating Student Allocations Table
 CREATE TABLE [dbo].[StudentAllocations](
-    AllocationID INT PRIMARY KEY  identity(1,1),
+    AllocationID INT PRIMARY KEY CLUSTERED  identity(1,1),
     Amount money NOT NULL,
     AllocationYear INT,
     StudentIDNum CHAR(13) REFERENCES [dbo].StudentsTable(StudentIDNum),
     StudentMarks INT, 
     ApplicationStatusID INT
-    CONSTRAINT FK_StudentAppStatus FOREIGN KEY (ApplicationStatusID) REFERENCES ApplicationStatuses(StatusID)
+    CONSTRAINT FK_StudentAppStatus FOREIGN KEY (ApplicationStatusID) REFERENCES ApplicationStatuses(StatusID),
+    CONSTRAINT CHK_AmountVal CHECK (Amount >= 0), -- no negatives 
+    CONSTRAINT CHK_AllocationYear CHECK (AllocationYear >= 0)
 );
 GO
 
@@ -175,10 +179,10 @@ CREATE TABLE [dbo].Documents (
 GO
 
 CREATE TABLE [dbo].TemporaryLinks (
-    LinkID INT PRIMARY KEY identity(1,1),
+    LinkID INT PRIMARY KEY CLUSTERED identity(1,1),
     StudentIDNum CHAR(13) REFERENCES [dbo].StudentsTable(StudentIDNum),
     LinkUrl NVARCHAR(1000), -- URL for link, 
-    ExpiryDate DATE, 
+    ExpiryDate DATE
     
 )
 
